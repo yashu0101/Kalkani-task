97f45e6cf31a6093f60a30ff4ad2b7222c6f495d
Yash Chavan <yashchavan61099@gmail.com>
2023-01-14 23:57:03 +0530

    ad

--- react-task/src/App.js
+++ react-task/src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import TestPagination from "./Component/TestPagination";
-import Task from "./Component/Task";
 
 function App() {
   return (
     <div className="App">
       <TestPagination />
-      {/* <Task /> */}
     </div>
   );
 }
